2d8dd11088a64786283c53a3c7b595bb8bd2c8b6
Nirilala Rakotondrasoa <nirilala15@gmail.com>
2024-11-04 14:10:42 +0100

    feat: add watch logic

--- taskify/src/task/task.rs
+++ taskify/src/task/task.rs
@@ -6,6 +6,7 @@ pub struct Task {
   pub title: String,
   pub description: Option<String>,
   pub state: String,
+  pub tags: Vec<String>,
 }
 
 impl Task {
@@ -20,6 +21,15 @@ impl Task {
     }).collect::<Vec<String>>()
   }
 
+  fn get_tags (str: &String) -> Vec<String> {
+    let mut tags = vec![];
+    let tags_regex = Regex::new(r"#[\w\d]+").unwrap();
+    for tag in tags_regex.find_iter(str) {
+      tags.push(tag.as_str().replace("#", ""));
+    }
+    return tags;
+  }
+
   fn get_state_and_title(title: &String) -> Option<(String, String)> {
     let splitted = title.split("]: ").collect::<Vec<&str>>();
     if splitted.len() < 2 {
@@ -32,12 +42,14 @@ impl Task {
 impl Transform<Option<Task>> for Task {
   fn transform(&self, str: String) -> Option<Task> {
     let mut sanitized = Task::sanitize(&str);
+    let tags = Task::get_tags(&str);
     if let Some((state, title)) = Task::get_state_and_title(&sanitized.remove(0)) {
       let description = sanitized.join("\r\n");
       return Some(Task { 
         title: title,
         description: if description.len() > 0 { Some(description) } else { None },
-        state
+        state,
+        tags,
       })
     }
     return Option::None;
