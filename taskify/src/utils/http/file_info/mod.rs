3dd71b33828fae52be55cb8ce197b72aaa884b74
Rakotondrasoa Nirilala <nirilala15@gmail.com>
2025-01-17 09:33:38 +0100

    fix: refactoring in get info from url

--- taskify/src/utils/http/file_info/mod.rs
+++ taskify/src/utils/http/file_info/mod.rs
@@ -1,19 +1,20 @@
+use reqwest::header::HeaderMap;
+
+
+fn extract_header<'a> (headers: &'a HeaderMap, key: &'a str) -> &'a str {
+  if let Some(value) = headers.get(key) {
+    return value.to_str().unwrap_or_default();
+  }
+  return "";
+}
 
 pub fn get_file_info_from_url (url: &str) -> Option<(String, u32)> {
   if let Ok(res) = reqwest::blocking::Client::new().head(url).send() {
     let headers = res.headers();
 
-    let content_type = if let Some(mime_type) = headers.get("content-type") { 
-      mime_type.to_str().unwrap_or_default() 
-    } else {
-      "unknown"
-    };
-
-    let size = if let Some(mime_type) = headers.get("content-length") { 
-      mime_type.to_str().unwrap_or_default().to_string().parse::<u32>().unwrap_or(0)
-    } else {
-      0
-    };
+    let content_type = extract_header(headers, "content-type");
+    let size = extract_header(headers, "content-length")
+    .to_string().parse::<u32>().unwrap_or(0);
 
     return Some((content_type.to_string(), size));
   }
